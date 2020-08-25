ee4c99f0a489e4a75491a0eb0f33bf436c92083f
bbkfed <balthazarberling+fed@gmail.com>
2020-08-25 13:23:02 +0200

    first connection between api and client

--- src/components/create/create.tsx
+++ src/components/create/create.tsx
@@ -1,12 +1,40 @@
-import React from "react";
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
+import Guest from "../../models/Guest";
 
 export default function Create() {
+  const defaultGuest: Guest = new Guest();
+  defaultGuest.name = "Pepito";
+  defaultGuest.email = "pepito@yahoo.lt";
+  defaultGuest.phone = "0765987458";
+
+  const [newGuest, setNewGuest] = useState(defaultGuest);
 
   function createAGuest() {
     const createUrl = "http://localhost:4000/create";
-    axios.get(createUrl).then(response => { console.log("it created." + response)});
+    axios({
+      method: "post",
+      url: createUrl,
+      data: {
+        name: defaultGuest.name,
+        email: defaultGuest.email,
+        phone: defaultGuest.phone
+      }
+    }).then((response) => {
+        console.log("it created.");
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  return <><button type="button" onClick={() => createAGuest() }>Create Guest</button></>
+  return (
+    <>
+      <button type="button" onClick={() => createAGuest()}>
+        Create Guest
+      </button>
+      <p>{JSON.stringify(newGuest)}</p>
+    </>
+  );
 }
