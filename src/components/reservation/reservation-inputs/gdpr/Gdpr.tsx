412fdde583f21a42c991b51049b429827f7e9d5e
Vic564 <victor.a.persson@gmail.com>
2020-09-02 15:38:28 +0200

    Added gdpr component

---
+++ src/components/reservation/reservation-inputs/gdpr/Gdpr.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Search.scss";
+
+export default function Contact() {
+  return (
+    <main>
+      <h3>INTEGRITETSINFORMATION FÖR BOKNING AV BORD PÅ L'OCEAN</h3>
+      <h5>Varför använder vi dina personuppgifter?</h5>
+      <p>Vi använder dina personuppgifter för att förbättra kvaliten i din kundupplevelse. 
+        Detta genom att bokningen lagras i ett databas-system och bindas till din person.</p>
+      <h5>Vad för slags personuppgifter samlar vi in?</h5>
+      <p>Vi samlar in så lite som möjligt av dina personuppgifter. Endast namn, e-post och telefonnummer(valfritt) är nödvändigt för detta ändamål.
+      </p>
+      <h5>Vem har tillgång till dina personuppgifter?</h5>
+      <p>Endast den bokningsansvarige på vår hemsida kan ta del av din information.</p>
+      <h5>Hur länge sparar vi dina uppgifter?</h5>
+      <p>Dina uppgifter sparas endast till den tidpunkt då besöket som bokningen avser är genomfört eller avbokat.</p>
+      <p>2/9 2020</p>
+    </main>
+  )
+}
