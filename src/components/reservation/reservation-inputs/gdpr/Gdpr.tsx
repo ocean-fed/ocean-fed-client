583de97df245b238b5d5640527d979aeafd5ad00
Vic564 <victor.a.persson@gmail.com>
2020-09-03 12:58:18 +0200

    added gdpr modal

--- src/components/reservation/reservation-inputs/gdpr/Gdpr.tsx
+++ src/components/reservation/reservation-inputs/gdpr/Gdpr.tsx
@@ -1,21 +1,72 @@
 import React from "react";
 import "./Gdpr.scss";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    position: 'absolute',
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 export default function Gdpr() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <main>
-      <h3>INTEGRITETSINFORMATION FÖR BOKNING AV BORD PÅ L'OCEAN</h3>
-      <h5>Varför använder vi dina personuppgifter?</h5>
-      <p>Vi använder dina personuppgifter för att förbättra kvaliten i din kundupplevelse. 
-        Detta genom att bokningen lagras i ett databas-system och bindas till din person.</p>
-      <h5>Vad för slags personuppgifter samlar vi in?</h5>
-      <p>Vi samlar in så lite som möjligt av dina personuppgifter. Endast namn, e-post och telefonnummer(valfritt) är nödvändigt för detta ändamål.
-      </p>
-      <h5>Vem har tillgång till dina personuppgifter?</h5>
-      <p>Endast den bokningsansvarige på vår hemsida kan ta del av din information.</p>
-      <h5>Hur länge sparar vi dina uppgifter?</h5>
-      <p>Dina uppgifter sparas endast till den tidpunkt då besöket som bokningen avser är genomfört eller avbokat.</p>
-      <p>2/9 2020</p>
-    </main>
-  )
+    <div>
+      <Button variant="contained" color="primary" onClick={handleOpen}>
+        Gdpr
+      </Button>
+
+      <Modal
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        open={open}
+        onClose={handleClose}
+      >
+        <div style={modalStyle} className={classes.paper}>
+        <h3>INTEGRITETSINFORMATION FÖR BOKNING AV BORD PÅ L'OCEAN</h3>
+        <h5>Varför använder vi dina personuppgifter?</h5>
+        <p>Vi använder dina personuppgifter för att förbättra kvaliten i din kundupplevelse. 
+          Detta genom att bokningen lagras i ett databas-system och bindas till din person.</p>
+        <h5>Vad för slags personuppgifter samlar vi in?</h5>
+        <p>Vi samlar in så lite som möjligt av dina personuppgifter. Endast namn, e-post och telefonnummer(valfritt) är nödvändigt för detta ändamål.
+        </p>
+        <h5>Vem har tillgång till dina personuppgifter?</h5>
+        <p>Endast den bokningsansvarige på vår hemsida kan ta del av din information.</p>
+        <p>2/9 2020</p>
+        </div>
+      </Modal>
+    </div>
+  );
 }
