9ac920bd7bddf319adc50f7f8d4d48473556b839
maklet <makletlindfors@gmail.com>
2020-09-10 00:22:27 +0200

    changed to correct names in contact

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -7,11 +7,11 @@ export default function Contact() {
     <main>
       <Box display="flex" justifyContent="center">
         <div className="contact-info">
-          <h3>Hej och välkommen till L'Ocean, din favorit fisk- och skaldjursrestaurang runt hörnet!</h3>
+          <h3>Hej och välkommen till L'Océan, din favorit fisk- och skaldjursrestaurang runt hörnet!</h3>
           <p>Du hittar oss på Högbergsgatan 62 på Södermalm, mitt i Stockholm. Vi erbjuder en liten och säsongsbaserad meny för att alltid garantera dig en matupplevelse utöver det vanliga med bästa möjliga råvaror. Har du några specifika frågor eller allergier så är du alltid välkommen att kontakta oss via telefon, numret hittar du nedan. Vi kan även ta emot bokningar över telefon, annars kan du boka ett bord <a href="/reservation">här.</a></p>
           <h4>Kontaktuppgifter</h4>
           <div className="contact-details">
-            <p>L'Ocean</p>
+            <p>L'Océan</p>
             <p>Högbergsgatan 62, 118 54 Stockholm</p>
             <p>08-640 82 13</p>
           </div>
