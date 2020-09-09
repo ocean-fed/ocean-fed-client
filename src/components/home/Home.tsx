79bc754f2cc419e2d30b778f67aac9d9cbda0834
Vic564 <victor.a.persson@gmail.com>
2020-09-10 00:29:52 +0200

    small change in style

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Box from '@material-ui/core/Box';
+import "./Home.scss";
 
 export default function Home() {
   return (
     <main>
       <Box display="flex" justifyContent="center">
-        <p>Välkommen till Restaurangen L'Océan!</p>
+        <p>Välkommen till L'Océan!</p>
       </Box>
     </main>
   )
