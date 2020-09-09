5ebdcc006be8c6bd52f46f993256cb0cd10d0450
Vic564 <victor.a.persson@gmail.com>
2020-09-09 14:07:49 +0200

    styling colors and buttons

--- src/components/reservation/reservation-inputs/gdpr/Gdpr.tsx
+++ src/components/reservation/reservation-inputs/gdpr/Gdpr.tsx
@@ -54,18 +54,29 @@ export default function Gdpr() {
         onClose={handleClose}
       >
         <div style={modalStyle} className={classes.paper}>
-          <h3>INTEGRITETSINFORMATION FÖR BOKNING AV BORD PÅ L'OCEAN</h3>
-          <h5>Varför använder vi dina personuppgifter?</h5>
-          <p>Vi använder dina personuppgifter för att förbättra kvaliten i din kundupplevelse. 
-            Detta genom att bokningen lagras i ett databas-system och därmed kan associeras till did som person.</p>
-          <h5>Vad för slags personuppgifter samlar vi in?</h5>
-          <p>Vi samlar in så lite som möjligt av dina personuppgifter. Endast namn, e-post och telefonnummer(valfritt) är nödvändigt för detta ändamål.
+          <h2>INTEGRITETSINFORMATION FÖR BOKNING AV BORD PÅ L'OCEAN</h2>
+          <h3>Varför använder vi dina personuppgifter?</h3>
+          <p>
+            Vi använder dina personuppgifter för att förbättra kvaliten i din
+            kundupplevelse. Detta genom att bokningen lagras i ett
+            databas-system och därmed kan associeras till did som person.
           </p>
-          <h5>Vem har tillgång till dina personuppgifter?</h5>
-          <p>Endast den bokningsansvarige på vår hemsida kan ta del av din information.</p>
+          <h3>Vad för slags personuppgifter samlar vi in?</h3>
+          <p>
+            Vi samlar in så lite som möjligt av dina personuppgifter. Endast
+            namn, e-post och telefonnummer(valfritt) är nödvändigt för detta
+            ändamål.
+          </p>
+          <h3>Vem har tillgång till dina personuppgifter?</h3>
+          <p>
+            Endast den bokningsansvarige på vår hemsida kan ta del av din
+            information.
+          </p>
           <p>2/9 2020</p>
           <Box display="flex" justifyContent="center">
-            <Button variant="outlined" onClick={handleClose}>Ok</Button>
+            <Button variant="contained" disableElevation color="primary" fullWidth onClick={handleClose}>
+              Ok
+            </Button>
           </Box>
         </div>
       </Modal>
